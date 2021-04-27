0850ed0bd898838848f48984465094f61c3cb9b7
Shauna Dunn <shaunadunn1@gmail.com>
2021-04-26 21:22:14 -0400

    form rendering / added navbar links

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import LoginButton from './components/LoginButton'
+import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
-
+import Navbar from "./components/Navbar";
+import AddNewHorse from "./components/AddNewHorse";
 
 function App() {
-    return (
+  return (
+    <Router>
       <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            Login page here
+          </Route>
+
+          <Route exact path="/addnewhorse">
+            <AddNewHorse />
+          </Route>
+        </Switch>
+      </div>
+
       <LoginButton />
       <LogoutButton />
-
-      
-      </div>
-    );
-  }
-
+    </Router>
+  );
+}
 
 export default App;
