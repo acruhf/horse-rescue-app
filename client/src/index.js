966778ac2453587362c464d781d23e61c4722f76
Shauna Dunn <shaunadunn1@gmail.com>
2021-04-30 16:02:18 -0400

    redirect working?

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+const uri='http://localhost:3000/callback';
+
 ReactDOM.render(
   <Auth0Provider
     domain="dev-atnvgqyo.us.auth0.com"
     clientId="y9zWm7KJPRo3m6ZTJItoQ3SqnLmtS2Ip"
-    redirectUri={window.location.origin}
+    redirectUri={uri}
   >
     <App />
   </Auth0Provider>,
