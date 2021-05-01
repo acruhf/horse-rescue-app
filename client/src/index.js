92f86c25651e0d4f16a12b5b9d88c16ff317007d
Austin Ruhf <austin.ruhf@gmail.com>
2021-05-01 10:22:56 -0400

    cleanup CRUD

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
