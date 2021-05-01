6c2ea5602efa0a3f55b8b32b01d98a888167a8d2
Austin Ruhf <austin.ruhf@gmail.com>
2021-04-30 22:15:41 -0400

    _id model change

--- server.js
+++ server.js
@@ -28,20 +28,20 @@ app.listen(PORT, () => {
 });
 
 
-const db = require("./models");
+// const db = require("./models");
 
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => {
+//     console.log("Connected to the database!");
+//   })
+//   .catch(err => {
+//     console.log("Cannot connect to the database!", err);
+//     process.exit();
+//   });
   
 // const express = require("express");
 // const mongoose = require("mongoose");
