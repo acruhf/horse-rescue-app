ab1a4dc91f21071e21465af7d7946ba70b3c302b
Amanda <awiehrs.photo@gmail.com>
2021-05-10 22:43:56 -0400

    no

--- client/build/precache-manifest.dc939dba79f131582e9a6f46c9af74c7.js
+++ client/build/precache-manifest.dc939dba79f131582e9a6f46c9af74c7.js
@@ -1,19 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "c4e4a78f1395e97be638",
-    "url": "/static/css/main.b1dba24a.chunk.css"
+    "revision": "6173a98d9ccf049e50b4",
+    "url": "/static/css/main.71195f27.chunk.css"
   },
   {
-    "revision": "c4e4a78f1395e97be638",
-    "url": "/static/js/main.a686725f.chunk.js"
+    "revision": "6173a98d9ccf049e50b4",
+    "url": "/static/js/main.2d81a654.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "d36a3e5ec146994e994e",
-    "url": "/static/js/2.4cd119c6.chunk.js"
+    "revision": "1aacc5c3dd5441f93b2b",
+    "url": "/static/js/2.3c0dcfce.chunk.js"
+  },
+  {
+    "revision": "775542599921f1ab64fc64b913b3304a",
+    "url": "/static/media/dash-header.77554259.jpg"
   },
   {
     "revision": "9ba195f5014b732f2bb2f40d698db135",
@@ -24,8 +28,8 @@ self.__precacheManifest = [
     "url": "/static/media/horseshoe-logo.9f011100.png"
   },
   {
-    "revision": "663b6c465f185a42ccf8246b826c3b8f",
-    "url": "/static/media/amanda-icon.663b6c46.png"
+    "revision": "8878e6117b191109d72feeeac70f78a8",
+    "url": "/static/media/amanda-icon.8878e611.png"
   },
   {
     "revision": "c70c893986359f3a2fd8e3178e550a2f",
@@ -34,10 +38,6 @@ self.__precacheManifest = [
   {
     "revision": "c30801832b8c0dd3397f0dd988c277d4",
     "url": "/static/media/shauna-icon.c3080183.png"
-  },
-  {
-    "revision": "775542599921f1ab64fc64b913b3304a",
-    "url": "/static/media/dash-header.77554259.jpg"
   },
   {
     "revision": "10a67ccad9bc283a3c800a3de4967347",
@@ -52,7 +52,7 @@ self.__precacheManifest = [
     "url": "/static/media/public-page-bg.861c5335.jpg"
   },
   {
-    "revision": "836afef840961065b3241528094ebf77",
+    "revision": "d9a15b74dba3dded1acff70ed174daa3",
     "url": "/index.html"
   }
 ];
