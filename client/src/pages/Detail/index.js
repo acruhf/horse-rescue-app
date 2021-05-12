eb112b71c681879d1077f5bacaecc7a7419f339a
Amanda <awiehrs.photo@gmail.com>
2021-05-11 21:58:49 -0400

    updated details page styling

--- client/src/pages/Detail/index.js
+++ client/src/pages/Detail/index.js
@@ -3,7 +3,8 @@ import { useParams } from "react-router-dom";
 import API from "../../utils/API";
 import Navbar from "../../components/Navbar";
 import VolunteerNav from "../../components/VolunteerNav"
-import HeaderImg from "../../assets/images/dash-header.jpg"
+import HeaderImg from "../../assets/images/details-header.jpg"
+import './style.css'
 
   // When this component mounts, grab the horse with the _id of props.match.params.id
   // e.g. localhost:3000/horses/599dcb67f0f16317844583fc
@@ -28,29 +29,32 @@ function Detail(props) {
                         <div className= "col-12 dashPg">
                             <img src={HeaderImg} alt="Horses in Desert" className="headerImg"></img>
                             <div className="currentPgHeader">
-                                Horse Details
-                            </div>
-                            
-                            <h1>
                                 {horse.name}
-                            </h1>
-                            <p>
-                                {horse.breed},
-                                <br></br>
-                                {horse.age} yrs,
-                                <br></br>
-                                {horse.height} hh,
-                                <br></br>
-                                Last Vet Appt: {horse.lastVetAppt},
-                                <br></br>
-                                Last Farrier Appt: {horse.farrierDate},
-                                <br></br>
-                                Farrier Cycle: {horse.farrierCycle},
-                                <br></br>
-                                Last Deworming: {horse.deworming},
-                                <br></br>
-                                Deworming Cycle: {horse.dewormingCycle},
-                            </p>
+                            </div>
+                            <div className="horseStats">
+                                <img src={horse.pictureUrl} alt="Horse" className="horsePic"></img>
+                                <p className="horseDetails">
+                                    
+                                    {horse.breed}
+                                    <br></br>
+                                    {horse.age}yrs old
+                                    <br></br>
+                                    {horse.height} hh
+                                    <br></br>
+                                    Last Vet Appt: {horse.lastVetAppt}
+                                    <br></br>
+                                    Last Farrier Appt: {horse.farrierDate}
+                                    <br></br>
+                                    Farrier Cycle: {horse.farrierCycle}
+                                    <br></br>
+                                    Last Deworming: {horse.deworming}
+                                    <br></br>
+                                    Deworming Cycle: {horse.dewormingCycle}
+                                    <br></br>
+                                    <br></br>
+                                    <a href="#" class="updateButton">Update Info</a>
+                                </p>
+                            </div>
                         </div>
                     </div>                  
                 </div>
