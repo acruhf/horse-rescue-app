0fa71baa5dc2a129cc7792e542a362068744d069
Austin Ruhf <austin.ruhf@gmail.com>
2021-05-15 15:27:14 -0400

    added preview gif to README

--- models/horse.js
+++ models/horse.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// Schema for Horse Database
 const HorseSchema = new Schema({
   name: { type: String },
   date: { type: Date, default: Date.now },
